7598af8e1cce935cb092a1273995edff4611afd2
Bagas Ranggen <bagas@logique.co.id>
2019-12-26 09:49:30 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <p className="text-center">MOVIEQ</p>
+  );
+}
+
+export default App;
